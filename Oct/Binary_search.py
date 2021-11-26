4e11c329b8d5b611650c9260b4fac2fb1d40c537
DarshusVish <vishal.a.saravade@gmail.com>
2021-11-26 20:42:00 +0530

    Binary search

---
+++ Oct/Binary_search.py
@@ -0,0 +1,32 @@
+p = -1
+
+def bsearch(list,n):
+    l = 0
+    u = len(list)-1
+    while l <= u:
+        mid = (l+u)//2
+
+        if list[mid] == n:
+            globals() ['p'] = mid
+            return True
+
+        else:
+            if list[mid] < n:
+                l = mid+1
+            else:
+                u = mid+1
+    return False
+
+
+ll = [x for x in range(1,1000)]
+n = 2000
+if bsearch(ll,n):
+    print("Found at",p+1)
+else:
+    print("Not found")
+
+a = 30
+b = 4
+
+c = 30 if a<b else 50
+print(c)
