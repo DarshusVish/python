a20dbfae000cbebd46956c4799a5eb20cee5ae41
DarshusVish <vishal.a.saravade@gmail.com>
2021-11-26 21:57:22 +0530

    Bubble and Selection sort

---
+++ Oct/Bubble_sort_and_Selection_sort.py
@@ -0,0 +1,25 @@
+
+def bsort(list):
+    for i in range(len(list)-1,0,-1):
+        for j in range(i):
+            if list[j]>list[j+1]:
+                temp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temp
+
+def ssort(list):
+    for i in range(len(list)-1):
+        m = i
+        for j in range(i,len(list)):
+            if list[j]< list[m]:
+                m = j
+        # print(list)
+        temp = list[i]
+        list[i] = list[m]
+        list[m] = temp
+
+
+ll = [2,4,1,9,5,3,7]
+ssort(ll)
+
+print(ll)
